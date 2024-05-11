9e5f1faf743e3a5ad0c710e9b6f4f26e03e945a6
David <dnhkng@gmail.com>
2024-05-11 08:55:31 +0100

    Feat Add espeak-ng installer

--- install_windows.bat
+++ install_windows.bat
@@ -1,4 +1,10 @@
 REM Download and install the required dependencies for the project on Windows
+
+echo Install espeak-ng...
+curl -L "https://github.com/espeak-ng/espeak-ng/releases/download/1.51/espeak-ng-X64.msi" --output "espeak-ng-X64.msi"
+espeak-ng-X64.msi
+del espeak-ng-X64.msi
+
 python -m venv venv
 call .\venv\Scripts\activate
 pip install -r requirements_cuda.txt
