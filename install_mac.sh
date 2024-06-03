d9537d6de3e03bab5e869b5f7f4e0afccb9709e2
ghostboo-124 <nat.perkins11@gmail.com>
2024-06-03 10:12:51 +1000

    Deleting the extra .txt file that was there Finishing first revision of the install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -4,31 +4,51 @@
 # https://github.com/dnhkng/GlaDOS
 
 # Installing espeak and Homebrew if neccessary
-echo Install espeak
-which -s brew
-if [[ $? != 0 ]] ; then
+echo Installing espeak and Homebrew if necessary
+if [[ $(command -v brew) == "" ]] ; then
     # Install Homebrew
     echo You do not have Homebrew installed, installing now
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
+    echo You have Homebrew installed, updating now
     brew update
 fi
 
 brew install espeak
 
-# Making espeak-ng become espeak in the tts.py
+# Making espeak-ng become espeak in the tts.py and disables CUDA
+echo Swaping the tts.py out for the one that uses espeak
 git clone https://github.com/Ghostboo-124/espeak-fix.git
-rm glados/tts.py
-cp espeak-fix/tts.py glados/tts.py
+rm glados/tts.py > /dev/null 2>&1
+cp espeak-fix/tts.py glados/tts.py > /dev/null 2>&1
 
-python3.12 -m venv venv
-source venv/bin/activate
-python3.12 -m pip install -r requirements.txt
+python3.12 -m venv venv > /dev/null 2>&1
+source venv/bin/activate > /dev/null 2>&1
+python3.12 -m pip install -r requirements.txt > /dev/null 2>&1
 
-# Downloading llama
-echo Downloading llama
-curl -L "https://github.com/ggerganov/llama.cpp/releases/download/b2839/llama-b2839-bin-macos-arm64.zip" --ouput "llama-b2839-bin-macos-arm64.zip"
+# Installing Whisper and llama
+echo Installing Whisper and llama
+git submodule update --init --recursive > /dev/null 2>&1
 
-# Unzipping llama
-echo Unzipping llama
-unzip llama-b2839-bin-macos-arm64.zip -d submodules/llama.cpp
+# Compiling Whisper
+echo Compiling Whisper
+cd submodules/whisper.cpp
+make libwhisper.so -j > /dev/null 2>&1
+cd ..
+cd ..
+
+# Compiling llama
+echo Compiling llama
+cd submodules/llama.cpp
+make server > /dev/null 2>&1
+cd ..
+cd ..
+
+# Downloading ASR and LLM models
+echo Downloading Models
+curl -L "https://huggingface.co/distil-whisper/distil-medium.en/resolve/main/ggml-medium-32-2.en.bin" --output  "models\ggml-medium-32-2.en.bin"
+curl -L "https://huggingface.co/bartowski/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct-Q6_K.gguf?download=true" --output "models\Meta-Llama-3-8B-Instruct-Q6_K.gguf"
+
+# Removing leftover files
+echo Cleaning up
+rm -rf espeak-fix
